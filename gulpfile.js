342b35a93b12db133ca3ef627603412d3749acd1
Tobias Viana <tobias.viana@usereserva.com>
2018-09-11 17:26:44 -0300

    Adding some minify functions for js/css/html

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,119 @@
-var fs = require("fs");
-var gulp = require('gulp');
-var del = require('del');
-var sass = require('gulp-sass');
-var nunjucksRender = require('gulp-nunjucks-render');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var browserSync = require('browser-sync').create();
+let gulp = require('gulp');
+let del = require('del');
+let sass = require('gulp-sass');
+let nunjucksRender = require('gulp-nunjucks-render');
+let browserify = require('browserify');
+let babelify = require('babelify');
+let source = require('vinyl-source-stream');
+let buffer = require('vinyl-buffer');
+let cleanCSS = require('gulp-clean-css');
+let uglify = require('gulp-uglify');
+let htmlmin = require('gulp-htmlmin');
+let browserSync = require('browser-sync').create();
 
-function _clean() {
-  return del(['dist']);
+let shared = {
+  clear: () => {
+    return del(['dist']);
+  },
+  assets: () => {
+    return gulp.src('src/assets/**/*')
+      .pipe(gulp.dest('dist/assets'))
+  },
+  help: (done) => {
+    console.info('###############################################################################')
+    console.info('')
+    console.info('Help:')
+    console.info('')
+    console.info('*gulp dev* to development mode')
+    console.info('*gulp build* to deployment mode')
+    console.info('*gulp clear* to clear dist folder')
+    console.info('')
+    console.info('###############################################################################')
+
+    done();
+  }
 }
 
-function _html() {
-  return gulp.src('src/*.html')
-    .pipe(nunjucksRender({
-      path: ['src/']
-    }))
-    .pipe(gulp.dest('dist'))
+let dev = {
+  html: () => {
+    return gulp.src('src/*.html')
+      .pipe(nunjucksRender({
+        path: ['src/']
+      }))
+      .pipe(gulp.dest('dist'))
+  },
+  styles: () => {
+    return gulp.src('src/scss/main.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('dist/css'));
+  },
+  scripts: () => {
+    return browserify({
+        entries: 'src/js/main.js',
+        debug: true
+      })
+      .transform(babelify.configure({
+        presets: ["@babel/preset-env"]
+      }))
+      .bundle()
+      .pipe(source('main.js'))
+      .pipe(gulp.dest('dist/js'));
+  },
+  reload: (done) => {
+    browserSync.reload()
+    return done();
+  },
+  serve: () => {
+    browserSync.init({
+      server: {
+        baseDir: "dist"
+      }
+    });
+
+    gulp.watch('src/js/**/*.js', gulp.series(dev.scripts, dev.reload))
+    gulp.watch('src/scss/**/*.scss', gulp.series(dev.styles, dev.reload))
+    gulp.watch('src/**/*.html', gulp.series(dev.html, dev.reload))
+  }
 }
 
-function _style() {
-  return gulp.src('src/scss/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/css'));
+let build = {
+  html: () => {
+    return gulp.src('src/*.html')
+      .pipe(nunjucksRender({
+        path: ['src/']
+      }))
+      .pipe(htmlmin({
+        collapseWhitespace: true
+      }))
+      .pipe(gulp.dest('dist'))
+  },
+  styles: () => {
+    return gulp.src('src/scss/main.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(cleanCSS())
+      .pipe(gulp.dest('dist/css'));
+  },
+  scripts: () => {
+    return browserify({
+        entries: 'src/js/main.js',
+        debug: true
+      })
+      .transform(babelify.configure({
+        presets: ["@babel/preset-env"]
+      }))
+      .bundle()
+      .pipe(source('main.js'))
+      .pipe(buffer())
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/js'));
+  }
 }
 
-function _scripts() {
-  return browserify({
-      entries: 'src/js/main.js',
-      debug: true
-    })
-    .transform(babelify.configure({
-      presets: ["@babel/preset-env"]
-    }))
-    .bundle()
-    .pipe(source('main.js'))
-    .pipe(gulp.dest('dist/js'));
-}
+//mainTasks exposed to gulp
+gulp.task('clear', shared.clear),
+gulp.task('help', shared.help),
+gulp.task('dev', gulp.series(shared.clear, gulp.parallel(dev.html, dev.styles, dev.scripts, shared.assets), dev.serve)),
+gulp.task('build', gulp.series(shared.clear, gulp.parallel(build.html, build.styles, build.scripts, shared.assets))),
 
-function _assets() {
-  return gulp.src('src/assets/**/*.*')
-    .pipe(gulp.dest('dist/assets'))
-}
-
-function _reload(done) {
-  browserSync.reload()
-  return done();
-}
-
-function _browserSync() {
-  browserSync.init({
-    server: {
-      baseDir: "dist"
-    }
-  });
-
-  gulp.watch('src/js/**/*.js', gulp.series(_scripts, _reload))
-  gulp.watch('src/scss/**/*.scss', gulp.series(_style, _reload))
-  gulp.watch('src/**/*.html', gulp.series(_html, _reload))
-}
-
-var build = gulp.series(_clean, gulp.parallel(_html, _style, _scripts, _assets));
-var dev = gulp.series(_clean, gulp.parallel(_html, _style, _scripts, _assets), _browserSync);
-
-gulp.task('clear', _clean)
-gulp.task('build', build)
-gulp.task('dev', dev)
-
-gulp.task('default', dev)
+//default dev
+gulp.task('default', shared.help)
